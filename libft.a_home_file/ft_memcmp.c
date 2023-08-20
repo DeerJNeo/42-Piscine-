9f40ec2241dd1d211c748444363d03080e0646b0
DeerJNeo <117808001+DeerJNeo@users.noreply.github.com>
2023-08-20 20:28:07 +0200

    ft

--- libft.a_home_file/ft_memcmp.c
+++ libft.a_home_file/ft_memcmp.c
@@ -11,3 +11,17 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
+int ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+    size_t  i;
+    
+    i = 0;
+    while (i < 0)
+    {
+        if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
+            return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+        i++;
+    }
+    return (0);
+}
