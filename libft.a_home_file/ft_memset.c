052b65270431c19fe5ccd4333022d6ecf004d292
DeerJNeo <117808001+DeerJNeo@users.noreply.github.com>
2023-08-21 20:07:30 +0200

    ft

--- libft.a_home_file/ft_memset.c
+++ libft.a_home_file/ft_memset.c
@@ -11,3 +11,16 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
+int *ft_memset(void *b, int c, size_t n)
+{
+    size_t i;
+
+    i = 0;
+    while (i < n)
+    {
+        ((unsigned char *)b)[i] = c;
+        i++;
+    }
+    return (b);
+}
--- libft.a_home_file/ft_putnbr_fd.c
+++ libft.a_home_file/ft_putnbr_fd.c
@@ -11,3 +11,19 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void    ft_putnbr_fd(int nb, int fd)
+{
+    unsigned int num;
+
+    if(nb < 0)
+    {
+        ft_putchar_fd('-', fd);
+        num = (unsigned int)(nb * -1);
+    }
+    else
+        num = (unsigned int)nb;
+    if (num >= 10)
+        ft_putnbr_fd(num / 10, fd);
+        ft_putchar_fd((char)(num % 10 + 48), fd);
+}
