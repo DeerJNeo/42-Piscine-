aeb6b9cd54c797ebb76e4bc08f041e18546b87d8
Kristian Jelinek <kjelinek@c1r1s1.42prague.com>
2023-08-24 15:37:30 +0200

    ft

--- libft.a_home_file/ft_calloc.c
+++ libft.a_home_file/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: kjelinek < kjelinek@student.42prague.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 13:53:30 by kjelinek          #+#    #+#             */
-/*   Updated: 2023/08/24 13:57:28 by kjelinek         ###   ########.fr       */
+/*   Updated: 2023/08/24 15:37:05 by kjelinek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	
+	void	*rtn;
+
+	rtn = malloc(size * count);
+	if (!rtn)
+		return NULL;
+	ft_bzero(rtn, count);
+	return (rtn);
 }
