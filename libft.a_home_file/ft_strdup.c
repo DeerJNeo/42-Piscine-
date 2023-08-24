a002a2c0294c8360734329d9bb7721ca2152dbc6
Kristian Jelinek <kjelinek@c1r1s1.42prague.com>
2023-08-24 13:49:03 +0200

    ft

---
+++ libft.a_home_file/ft_strdup.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kjelinek < kjelinek@student.42prague.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/24 13:47:11 by kjelinek          #+#    #+#             */
+/*   Updated: 2023/08/24 13:47:31 by kjelinek         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*rtn;
+	size_t	len;
+
+	len = ft_strlen(s) + 1;
+	rtn = malloc(sizeof(char) * len);
+	if (!rtn)
+		return (0);
+	rtn = ft_memcpy(rtn, s, len);
+	return (rtn);
+}
