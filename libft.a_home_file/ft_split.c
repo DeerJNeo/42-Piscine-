ba5aff1dbc4dacf9e2a5e3f126ec13fb5cfc4769
Kristian Jelinek <kjelinek@c1r1s1.42prague.com>
2023-08-25 14:30:29 +0200

    ft

--- libft.a_home_file/ft_split.c
+++ libft.a_home_file/ft_split.c
@@ -6,110 +6,78 @@
 /*   By: kjelinek < kjelinek@student.42prague.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 12:11:20 by kjelinek          #+#    #+#             */
-/*   Updated: 2023/08/25 12:24:21 by kjelinek         ###   ########.fr       */
+/*   Updated: 2023/08/25 14:29:17 by kjelinek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	void	ft_freeup(char **strs)
-
+static  int count_strings(char const *s, char c)
 {
-	int i;
-	
-	i = 0;
-	while (strs[i] != NULL)
-	{
-		free(strs);
-		i++;
-	}
-	free(strs);
+    int i;
+    int count;
+
+    count = 0;
+    i = 0;
+    while (s[i])
+    {
+        while (s[i] && s[i] == c)
+            i++;
+        if (s[i])
+            count++;
+        while (s[i] && s[i] != c)
+            i++;
+    }
+    return (count);
 }
 
-static	int	ft_word_count(char *str, char c)
-
+static  int str_sep_len(char const *s, char c)
 {
-	int	i;
-	int	word;
+    int i;
 
-	i = 0;
-	word = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] != c)
-		{
-			word++;
-			while (str[i] != '\0' && str[i] != c)
-				i++;
-			if (str[i] == '\0')
-				return (word);
-		}
-		i++;
-	}
-	return (word);
+    i = 0;
+    while (s[i] && s[i] != c)
+        i++;
+    return (i);
 }
 
-static	 void	ft_strcpy(char *word, char *str, char c, int j)
-
+static  char    *str_sepdup(char const *s, char c)
 {
-	int	i;
-	
-	i = 0;
-	while (str[j] != '\0' && str[j] != c)
-		j++;
-	while (str[j + i] != c && str[j + i] != '\0')
-	{
-		word[i] = str[j + i];
-		i++;
-	}
-	word[i] = '\0';
+    char    *word;
+    int     i;
+    int     len;
+
+    i = 0;
+    len = str_sep_len(s, c);
+    word = (char *)malloc((len + 1) * sizeof(char));
+    if (!word)
+        return (NULL);
+    while (i < len)
+    {
+        word[i] = s[i];
+        i++;
+    }
+    word[i] = '\0';
+    return (word);
 }
 
-static	void	ft_stralloc(char *str, char c, int *k)
-
+char    **ft_split(char const *s, char c)
 {
-	char	*word;
-	int		j;
+    char    **strings;
+    int     i;
 
-	j = *k;
-	word = NULL;
-	while (str[*k] != '\0' && str[*k] == c)
-		*k += 1;
-	while (str[*k] != '\0' && str[*k] != c)
-		*k += 1;
-	word = (char *)malloc(sizeof(char) * (*k - j + 1));
-	if (word == NULL)
-		return ;
-	ft_strcpy(word, str, c, j);
-	return ;
-}
-
-char	**ft_split(char const *str, char c)
-{
-	char	**strs;
-	int	i;
-	int	j;
-	int	position;
-
-	if (str == NULL)
-		return NULL;
-
-	i = 0;
-	position = 0;
-	j = ft_word_count((char *)str, c);
-	strs = (char **)malloc(sizeof(char *) * (j + 1));	
-	if (strs == NULL)
-		return (NULL);
-	strs[j] = NULL;
-	while (i < j)
-	{
-		ft_stralloc((char *)str, c, &position);
-		if (strs[i] == NULL)
-		{
-			ft_freeup(strs);
-			return NULL;
-		}
-		i++;
-	}
-	return (strs);	
+    i = 0;
+    strings = (char **)malloc(sizeof(char *)
+            * (count_strings(s, c) + 1));
+    while (*s)
+    {
+        while (*s && *s == c)
+            s++;
+        if (*s)
+            strings[i++] = str_sepdup(s, c);
+        while (*s && *s != c)
+            s++;
+    }
+    strings[i] = 0;
+    return (strings);
 }
